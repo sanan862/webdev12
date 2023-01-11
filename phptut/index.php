68cf77ad999c86686cd19c2a4c4e8f228b802256
sanan862 <Sanan.Hanif@student.shu.ac.uk>
2023-01-11 18:48:23 +0000

    first commit

---
+++ phptut/index.php
@@ -0,0 +1,11 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$conn=new mysqli( $servername,$username,$password);
+if($conn->connect_error){
+    die("connection failed".$conn->connect_error);
+}
+echo "connection succesful";
+$conn->close();
+?>
